2876f8035c4778f506c48f4f3c01e1da5027a5bd
jetiny <86287344@qq.com>
2023-02-09 13:19:07 +0800

    feat: 添加demo

--- demo/main.go
+++ demo/main.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jetiny/sgin"
-	"github.com/jetiny/sgin/base"
 	"github.com/jetiny/sgin/common"
+	"github.com/jetiny/sgin/uses"
 )
 
 func main() {
-	ctx := sgin.MustBoot(base.BootWithNone)
+	ctx := sgin.MustBootAll()
+	// ctx := sgin.MustBoot(base.BootWithNone)
 	r := gin.Default()
+	ctx.AppModdelHandle = func(c *gin.Context, appCode string) *common.AppModel {
+		return &common.AppModel{
+			AppId:   1,
+			AppCode: "8888",
+			AppName: "8888",
+		}
+	}
+	ctx.TokenHandle = func(c *gin.Context, token string) *common.UserToken {
+		return &common.UserToken{
+			AccessToken:      token,
+			RefreshToken:     token,
+			AccessExpiredAt:  time.Now().Add(time.Hour * 100),
+			RefreshExpiredAt: time.Now().Add(time.Hour * 100),
+		}
+	}
+	ctx.WithRoutes([]*common.Route{
+		{Method: http.MethodPost, Path: "/user/profile/info", EnsureAuth: true, Handle: func(ctx *gin.Context) {
+			sgin.GetCtx(ctx).Pass()
+		}},
+	})
 	ctx.Install(r, func(ctx *common.BootContext, r *gin.Engine) {
 		r.GET("/pass", func(c *gin.Context) {
 			sgin.GetCtx(c).Pass()
 		})
-		r.GET("/success", func(c *gin.Context) {
-			sgin.GetCtx(c).Success("OK")
-		})
 	})
+	uses.Setup(ctx, r)
 	ctx.PrintAddr()
 	r.Run(ctx.Addr)
 }
