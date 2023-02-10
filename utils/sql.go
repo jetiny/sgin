196d2efd9aacd94434d58d6e59edfc145295e5ae
jetiny <86287344@qq.com>
2023-02-10 10:18:37 +0800

    feat: add sql field

--- utils/sql.go
+++ utils/sql.go
@@ -41,3 +41,59 @@ func SqlWhereLike(field string) string {
 func SqlWherePrefix(field string) string {
 	return "locate(?, " + field + ") = 1"
 }
+
+type SqlFieldType string
+
+func Field(field string) SqlFieldType {
+	return SqlFieldType(field)
+}
+
+func (s SqlFieldType) Eq(value any) (string, any) {
+	return string(s) + " = ? ", value
+}
+
+func (s SqlFieldType) Neq(value any) (string, any) {
+	return string(s) + " <> ? ", value
+}
+
+func (s SqlFieldType) Gt(value any) (string, any) {
+	return string(s) + " > ? ", value
+}
+
+func (s SqlFieldType) Gte(value any) (string, any) {
+	return string(s) + " >= ? ", value
+}
+
+func (s SqlFieldType) Lt(value any) (string, any) {
+	return string(s) + " < ? ", value
+}
+
+func (s SqlFieldType) Lte(value any) (string, any) {
+	return string(s) + " <= ? ", value
+}
+
+func (s SqlFieldType) NotIsNull() string {
+	return string(s) + " not is null "
+}
+
+func (s SqlFieldType) IsNull() string {
+	return string(s) + " is null "
+}
+
+func (s SqlFieldType) Like(value any) (string, any) {
+	return string(s) + " like ? ", value
+}
+
+func (s SqlFieldType) Prefix(value any) (string, any) {
+	return "locate(?, " + string(s) + ") = 1", value
+}
+
+// xorm.OrderBy field desc
+func (s SqlFieldType) OrderDesc() string {
+	return "`" + string(s) + "` desc "
+}
+
+// xorm.OrderBy field asc
+func (s SqlFieldType) OrderAsc() string {
+	return "`" + string(s) + "` asc "
+}
