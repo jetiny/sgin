b95bc2de75861a3a0df0819900b062d46cee8154
jetiny <86287344@qq.com>
2024-02-08 14:51:28 +0800

    add  logModule

--- common/logger.go
+++ common/logger.go
@@ -3,3 +3,20 @@ package common
 import "github.com/sirupsen/logrus"
 
 var Logger *logrus.Logger = logrus.New()
+
+type LogModule interface {
+	Info(args ...interface{})
+	Warn(args ...interface{})
+	Error(args ...interface{})
+	Debug(args ...interface{})
+	Trace(args ...interface{})
+	Infof(format string, args ...interface{})
+	Warnf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+	Debugf(format string, args ...interface{})
+	Tracef(format string, args ...interface{})
+}
+
+func NewLogModule(name string) LogModule {
+	return Logger.WithField("module", name)
+}
