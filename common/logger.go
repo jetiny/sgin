780d89effa8edc135c32f47574b9e455f9813c4e
jetiny <86287344@qq.com>
2024-02-10 20:23:52 +0800

    restore log

--- common/logger.go
+++ common/logger.go
@@ -4,19 +4,6 @@ import "github.com/sirupsen/logrus"
 
 var Logger *logrus.Logger = logrus.New()
 
-type LogModule interface {
-	Info(args ...interface{})
-	Warn(args ...interface{})
-	Error(args ...interface{})
-	Debug(args ...interface{})
-	Trace(args ...interface{})
-	Infof(format string, args ...interface{})
-	Warnf(format string, args ...interface{})
-	Errorf(format string, args ...interface{})
-	Debugf(format string, args ...interface{})
-	Tracef(format string, args ...interface{})
-}
-
-func NewLogModule(name string) LogModule {
+func NewLogModule(name string) *logrus.Entry {
 	return Logger.WithField("module", name)
 }
