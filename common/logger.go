ddd60d4673b690926229c1a839d0c3e67eaa8280
jetiny <86287344@qq.com>
2024-02-10 22:21:59 +0800

    add many

--- common/logger.go
+++ common/logger.go
@@ -7,3 +7,12 @@ var Logger *logrus.Logger = logrus.New()
 func NewLogModule(name string) *logrus.Entry {
 	return Logger.WithField("module", name)
 }
+
+func NewLogModules(name string, subs ...string) []*logrus.Entry {
+	logs := make([]*logrus.Entry, len(subs)+1)
+	logs[0] = NewLogModule(name)
+	for i, sub := range subs {
+		logs[i] = NewLogModule(name + "." + sub)
+	}
+	return logs
+}
